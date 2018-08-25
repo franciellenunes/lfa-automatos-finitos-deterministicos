493c3af4d4afc09837e7f120c3d95c456fdee2ce
Luís Fernando de Ávila <luisfernandoavila92@gmail.com>
2018-08-25 13:52:47 -0300

    Implementação da estrutura do simulador

---
+++ Automatos/src/br/com/iftm/lfa/controller/A.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.iftm.lfa.controller;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author luis
+ */
+public class A {
+    public A(String cadeia){
+         JOptionPane.showMessageDialog(null, cadeia);
+    }
+   
+    
+}
