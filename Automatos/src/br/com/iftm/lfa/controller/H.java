ace4eb20299343d50ee9d02a99adc7bdbc71a222
franciellenunes <franciellesnunes@gmail.com>
2018-08-26 11:38:40 -0300

    Adicionando resolução de exercícios das letras H e J

--- Automatos/src/br/com/iftm/lfa/controller/H.java
+++ Automatos/src/br/com/iftm/lfa/controller/H.java
@@ -1,24 +1,78 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * MIT License - Copyright (c) 2018 Francielle da Silva Nunes, Luís Fernando Ávila
+ * Criada em 25 ago 2018
  */
 package br.com.iftm.lfa.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
+ * Classe criada para resolução do exercício correspondente a letra H 
+ * A linguagem inicia-se com 0 e a soma de todos os seus dígitos é par Ou
+ * inicia-se com 1 e a soma de todos os seus dígitos é ímpar
  *
- * @author luis
+ * @author Francielle da Silva Nunes
  */
 public class H {
 
-    public H(char[] cadeia) {
-        Boolean erro = false;
-        int Q = 1;
-        for (char c : cadeia) {
-            switch (Q) {
-            }
+    public H(char[] cadeia) {       
+
+        List<Character> digitos = new ArrayList<Character>();
+        for (int i = 0; i <= 9; i++) {                        
+            digitos.add((char)(i+'0'));
         }
 
-    }
+        List<Character> pares = new ArrayList<Character>();
+        for (int i = 0; i < 9; i += 2) {
+            pares.add((char)(i+'0'));
+        }
 
+        List<Character> impares = new ArrayList<Character>();
+        for (int i = 1; i <= 9; i += 2) {
+            impares.add((char)(i+'0'));
+        }
+
+        Boolean erro = false;
+        int Q = 1;
+        for (char c : cadeia) {  
+            switch (Q) {                
+                case 1:
+                    if (c == '0' || c == '1') {
+                        Q = 2;                       
+                    } else {
+                        erro = true;
+                    }
+                    break;
+                case 2:
+                    if (pares.contains(c)) {                        
+                        Q = 2;
+                    } else if (impares.contains(c)) {
+                        Q = 3;
+                    } else {
+                        erro = true;
+                    }
+                    break;
+                case 3:
+                    if (pares.contains(c)) {
+                        Q = 3;
+                    } else if (impares.contains(c)) {
+                        Q = 2;
+                    } else {
+                        erro = true;
+                    }
+                    break;
+                case 4:
+                    erro = true;
+                default:
+                    break;
+            }
+        }
+        if (erro || (Q != 2)) {
+            JOptionPane.showMessageDialog(null, "Sentença não reconhecida");
+        } else {
+            JOptionPane.showMessageDialog(null, "Sentença reconhecida");
+        }
+    }
 }
--- Automatos/src/br/com/iftm/lfa/controller/J.java
+++ Automatos/src/br/com/iftm/lfa/controller/J.java
@@ -1,22 +1,113 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * MIT License - Copyright (c) 2018 Francielle da Silva Nunes, Luís Fernando Ávila
+ * Criada em 25 ago 2018
  */
 package br.com.iftm.lfa.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
+ * Classe criada para resolução do exercício correspondente a letra J 
+ * Linguagem é um número inteiro, um número decimal ou um número representado em
+ * notação científica
  *
- * @author luis
+ * @author Francielle da Silva Nunes
  */
 public class J {
 
     public J(char[] cadeia) {
+
+        List<Character> digitos = new ArrayList<Character>();
+        for (int i = 0; i <= 9; i++) {
+            digitos.add((char) (i + '0'));
+        }
+
         Boolean erro = false;
         int Q = 1;
         for (char c : cadeia) {
             switch (Q) {
+                case 1:
+                    if (c == '+' || c == '-') {
+                        Q = 2;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 2:
+                    if (digitos.contains(c)) {
+                        Q = 3;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 3:
+                    if (digitos.contains(c)) {
+                        Q = 3;
+                    } else if (c == ',') {
+                        Q = 4;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 4:
+                    if (digitos.contains(c)) {
+                        Q = 5;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 5:
+                    if (digitos.contains(c)) {
+                        Q = 5;
+                    } else if (c == 'e') {
+                        Q = 6;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 6:
+                    if (c == '+' || c == '-') {
+                        Q = 7;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 7:
+                    if (digitos.contains(c)) {
+                        Q = 8;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 8:
+                    if (digitos.contains(c)) {
+                        Q = 8;
+                    } else {
+                        Q = 9;
+                        erro = true;
+                    }
+                    break;
+                case 9:
+                    erro = true;
+                    break;
+                default:
+                    break;
             }
+        }
+        if (erro || (Q != 3 && Q != 5 && Q != 8)) {
+            JOptionPane.showMessageDialog(null, "Sentença não reconhecida");
+        } else {
+            JOptionPane.showMessageDialog(null, "Sentença reconhecida");
         }
 
     }
